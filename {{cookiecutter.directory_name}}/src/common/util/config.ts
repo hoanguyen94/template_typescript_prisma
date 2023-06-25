5237607c5bdaeab34d9033ff51ee523045341ac1
Nguyen <hoa.nguyen@sap.com>
2023-06-25 18:51:25 +0200

    fix port

--- {{cookiecutter.directory_name}}/src/common/util/config.ts
+++ {{cookiecutter.directory_name}}/src/common/util/config.ts
@@ -9,14 +9,7 @@ const {
   QUEUEURL,
   ACCESSKEYID,
   SECRETACCESSKEY,
-  VNP_VERSION,
-  VNP_TMNCODE,
-  VNP_SECRETKEY,
-  VNP_URL,
-  VNP_RETURNURL_CREATEPAYMENT,
-  VNP_AFTERPAYMENT_URL,
   REDISTTL,
-  PROTOCOL,
 } = process.env;
 
 export default () => ({
@@ -36,17 +29,8 @@ export default () => ({
   database_url:
     DATABASE_URL ||
     'postgresql://{{cookiecutter.postgres_user}}:{{cookiecutter.postgres_password}}@localhost:{{cookiecutter.db_port}}/{{cookiecutter.db_name}}?schema=public',
-  port: PORT || 3334,
+  port: PORT || 8080,
   broker_url: BROKERRURL || 'localhost:9092',
   redisUrl: REDISURL || 'redis://:skindex2023@localhost:6379',
   redisTtl: REDISTTL || 10,
-  vnpay: {
-    vnp_Version: VNP_VERSION || '2.1.0',
-    vnp_TmnCode: VNP_TMNCODE || 'SKDEX001',
-    vnp_secretKey: VNP_SECRETKEY || 'SJXRLJURDYWQJCYGXCZYYQNHVYACUPFB',
-    vnp_Url: VNP_URL || 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html',
-    vnp_ReturnURL_CreatePayment:
-      VNP_RETURNURL_CREATEPAYMENT || 'https://www.google.com/',
-  },
-  protocol: PROTOCOL || 'http',
 });
